e7241694265c7087c8829428c246e4c956e2e0b6
alex.liu <alex.liu@idsuipai.com>
2018-04-12 23:04:52 +0800

    增加promise-primitive最原始的promise实现方法

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my-promise
-my own promise project
+我自己的promise实现，含有两个promise文件。
+
+- promise-primitive是比较粗略简单的一个实现，仅仅用作分析promise内部原理。
+- promise是比较符合promise标准的一个实现，含有一些拓展API，基本能够满足平时使用的需求。
+
+具体的分析可以参见我的博客 [https://insaneblue.github.io/](https://insaneblue.github.io/)
---
+++ promise-primitive.js
@@ -0,0 +1,67 @@
+// promise定义
+function Promise(fn) {
+  var state = 'pending',
+    value = null,
+    callbacks = [];
+
+  this.then = function (onFulfilled, onRejected) {
+    return new Promise(function (resolve, reject) {
+      handle({
+        onFulfilled: onFulfilled || null,
+        onRejected: onRejected || null,
+        resolve: resolve,
+        reject: reject
+      });
+    });
+  };
+
+  function handle(callback) {
+    if (state === 'pending') {
+      callbacks.push(callback);
+      return;
+    }
+
+    var cb = state === 'fulfilled' ? callback.onFulfilled : callback.onRejected,
+      ret;
+    if (cb === null) {
+      cb = state === 'fulfilled' ? callback.resolve : callback.reject;
+      cb(value);
+      return;
+    }
+    try {
+      ret = cb(value);
+      callback.resolve(ret);
+    } catch (e) {
+      callback.reject(e);
+    }
+  }
+
+  function resolve(newValue) {
+    if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+      var then = newValue.then;
+      if (typeof then === 'function') {
+        then.call(newValue, resolve, reject);
+        return;
+      }
+    }
+    state = 'fulfilled';
+    value = newValue;
+    final();
+  }
+
+  function reject(reason) {
+    state = 'rejected';
+    value = reason;
+    final();
+  }
+
+  function final() {
+    setTimeout(function () {
+      callbacks.forEach(function (callback) {
+        handle(callback);
+      });
+    }, 0);
+  }
+
+  fn(resolve, reject);
+}
